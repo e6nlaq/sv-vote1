2efcd7533f4d6bb76003a8aa72205bc4bbc09172
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-03-07 17:52:49 +0900

    r

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const scloudjs = require("scloudjs"); //scloudjsをモジュールとして使えるようにする
 let clouddatas = new Object();//このオブジェクトにクラウド変数のデータが入る
 
+// ユーザーリスト
+var userlist = [];
+
 const main = (data) => {//メッセージを受け取ったときにどんな処理をするかを設定する
-    const temp = scloudjs.parsedata(data, clouddatas);//受け取ったメッセージを整理する
-    clouddatas = temp.clouddatas;//クラウド変数のデータ
-    const changedlists = temp.changedlists;//変更された変数一覧
-    // scloudjs.sendtocloud("HOST", 19);//変数HOSTを19にする
+	const temp = scloudjs.parsedata(data, clouddatas);//受け取ったメッセージを整理する
+	clouddatas = temp.clouddatas;//クラウド変数のデータ
+	const changedlists = temp.changedlists;//変更された変数一覧
+	// scloudjs.sendtocloud("HOST", 19);//変数HOSTを19にする
 
-    console.log(`ID: ${clouddatas["usr"].value}, REQ0`);
+	const id = clouddatas["usr"].value.substr(0, 6);
+	const dat = clouddatas["usr"].value;
 
+	switch (dat[6]) {
+		case "0":
+			scloudjs.sendtocloud("sev", id + "1");
+			console.log(`Request 0: ${id}`);
+			break;
+
+		default:
+			console.log(`Inveid Request: ${dat[6]}`);
+			break;
+	}
 
 };
 
@@ -16,8 +30,8 @@ scloudjs.setdatas(process.env.username, process.env.password, process.env.projec
 
 const func = async () => {//実行
 
-    await scloudjs.login();//scratchにログイン
-    await scloudjs.connect();//scratchのクラウド変数サーバーに接続
-    await scloudjs.handshake();//プロジェクトに接続
+	await scloudjs.login();//scratchにログイン
+	await scloudjs.connect();//scratchのクラウド変数サーバーに接続
+	await scloudjs.handshake();//プロジェクトに接続
 };
 func();
